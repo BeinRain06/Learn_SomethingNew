d123e41d49a22e1af67a6e57fd7f3b0f8a8290a7
Gerard_Neo <elngoueni@gmail.com>
2022-05-13 17:50:30 +0200

    java commit 1

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,33 +1,55 @@
+
 var play=document.getElementById('play');
-var range=document.getElementById('play');
-var videoTime=document.getElementById('play');
-var videoDuration=document.getElementById('play');
-var life_video=document.getElementById('play');
+var range=document.getElementById('range');
+var videoTime=document.getElementById('videoTime');
+var videoDuration=document.getElementById('videoDurztion');
+var life_video=document.getElementById('life_video');
 
 // add EventListeners
 play.addEventListener('click', function(){
-  if (life_video.paused){
+  if (life_video.paused()){
     life_video.play();
-    document.querySelector('#play i').classList.replace('fa-play', 'fa-pause');
+    document.querySelector('#play ').innerText='pause';
   }
   else{
     life_video.pause();
-    document.querySelector('#play i').classList.replace('fa-pause', 'fa-play');
+    document.querySelector('#play ').innerText='play';
   }
 })
 
 range.addEventListener('input', function(){
-  life_video.currenTime = range.value;
-  range.max = Math.floor(life_video.duration);
+  life_video.currentTime() = range.value();
+  range.max() = Math.floor(life_video.duration());
 })
 
-life_video.addEventListener('timeupdate', function(){
-  videoTime.innerHTML= life_video.currenTime;
-  range.value = life_video.currenTime;
-  range.max = Math.floor(life_video.duration);
+life_video.addEventListener('time update ', function(){
+  videoTime.innerHTML()= life_video.currenTime();
+  range.value() = life_video.currenTime();
+  range.max() = Math.floor(life_video.duration());
 })
 
 window.onload = function(){
-  videoTime.innerHTML= life_video.currenTime;
-  videoDuration.innerHTML = Math.floor(life_video.duration);
+  videoTime.innerHTML()= life_video.currenTime();
+  videoDuration.innerHTML() = Math.floor(life_video.duration());
 }
+
+
+// navbar 
+
+    const doc = document.querySelector('html');
+    let nav_link = document.querySelector('.navbar ul li a');
+    let sibling = document.querySelectorAll('.navbar ul li a ') ;
+    let siblings = sibling.parentNode;
+
+    nav_link.addEventListener('click', function(){
+      
+      for( let i=0; i<siblings.length; i++); {
+        if(sibling[i] !== this.nav_link) {
+           this.nav_link.classList.toggle('current');
+
+        }
+        else{
+          this.nav_link.classList.toggle('current');
+        }
+    }
+    })
